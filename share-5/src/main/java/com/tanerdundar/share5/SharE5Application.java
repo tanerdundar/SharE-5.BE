35bcfd67643c951a0b4aaa1380c918a6ec851b5c
tanerdundar <t.dundar145@gmail.com>
2023-04-19 16:22:16 +0300

    Initial commit

---
+++ share-5/src/main/java/com/tanerdundar/share5/SharE5Application.java
@@ -0,0 +1,13 @@
+package com.tanerdundar.share5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharE5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharE5Application.class, args);
+	}
+
+}
---
+++ share-5/src/test/java/com/tanerdundar/share5/SharE5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanerdundar.share5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharE5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
